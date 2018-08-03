4c6d697894ad3f8daa83c78d241ac34e55314111
Chris <christoleavenow@gmail.com>
2018-08-03 20:14:34 +0800

    update get-json.py

--- get-json.py
+++ get-json.py
@@ -28,7 +28,7 @@ def get_html(q, page):
     try:
         response = requests.get(new_url, headers=headers)
         if response.status_code == 200:
-            result = json.dumps(response.json(), indent=4)
+            result = json.dumps(response.json(), indent=4, ensure_ascii=False)
             return result
     except requests.ConnectionError:
         return None
